8aa5262482ecaf4284ddb31f1555e0954112d41d
relaxsbk.exe <pah4n1989@gmail.com>
2023-10-19 00:06:16 +0800

    сделана форма без методов 25 34

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,21 @@
 </script>
 
 <template>
-<div>
+<div class="app">
   <div>
-    Кол-во лайков <strong>{{ likes }}</strong>
+    <form >
+      <h4>Создание поста</h4>
+      <input class="input" type="text" placeholder="Название"><br>
+      <input class="input" type="text" placeholder="Описание"><br>
+      <button class="btn">Создать</button>
+    </form>
   </div>
-  <div>
-    Кол-во дизлайков <strong>{{ dislikes }}</strong>
+  <div class="post" v-for="post in posts">
+    <div> <strong>Название:</strong> {{post.title}} </div>
+    <div> <strong>Описание:</strong> {{post.description}} </div>
+
   </div>
-  <button @click="addLike">like</button>
-  <button @click="addDisLike">dislike</button>
+
 </div>
 </template>
 
@@ -19,21 +25,55 @@
 export default {
   data() {
     return {
-      likes: 0,
-      dislikes: 0,
+      posts: [
+        {id: 1, title: 'JS', description: 'Описание лоол'},
+        {id: 2, title: 'JS 2', description: 'Описание лоол 2'},
+        {id: 3, title: 'JS 3', description: 'Описание лоол 3'},
+        {id: 4, title: 'JS 4', description: 'Описание лоол 4'},
+      ]
     }
   },
   methods: {
-    addLike() {
-      this.likes += 1;
-    },
-    addDisLike() {
-      this.dislikes += 1;
-    },
+
   }
 }
 </script>
 
-<style scoped>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+.app {
+  padding: 20px;
+}
+
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 50%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: center;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
 </style>
