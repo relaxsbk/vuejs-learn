2acff7bda99655ab7f57e41baf929edc7f7131ae
relaxsbk.exe <pah4n1989@gmail.com>
2023-10-18 23:40:18 +0800

    Первый объяснённый прикол с компонентом

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
+<script setup>
+
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<div>
+  <div>
+    Кол-во лайков <strong>{{ likes }}</strong>
+  </div>
+  <div>
+    Кол-во дизлайков <strong>{{ dislikes }}</strong>
+  </div>
+  <button @click="addLike">like</button>
+  <button @click="addDisLike">dislike</button>
+</div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDisLike() {
+      this.dislikes += 1;
+    },
+  }
 }
+</script>
 
-nav {
-  padding: 30px;
-}
+<style scoped>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
